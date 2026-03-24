a2c577da9d63523b3970c43794f2839d9e466981
kmarci116 <21d_kantora@nyirszikszi.hu>
2026-03-24 16:20:22 +0100

    create mariadb connection pool in db.js

---
+++ db.js
@@ -0,0 +1,18 @@
+const mariadb = require('mariadb');
+const path = require('path');
+
+require('dotenv').config({ path: path.join(__dirname, '.env')});
+require('dotenv').config({ path: path.join(__dirname, '../.env')});
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 5
+})
+
+module.exports ={
+    query: (text, params) => pool.query(text, params),
+    pool: pool
+};
